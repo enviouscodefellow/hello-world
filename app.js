62b3ddc33f3ca43ef3ee4e72da6f2374405cbcd9
Nick Seidel <enviouscodefellow@outlook.com>
2022-09-01 15:15:48 -0700

    Added user prompt for building houses

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-'use strict'; //proper rules and no rule breaking
+"use strict"; //proper rules and no rule breaking
 
-let userName;
-userName = prompt('What is your name?');
-if (userName.length == 0) {
-  userName = 'user';
-  console.log('Hello ' + userName + '!');
-}
-else {
-  console.log('Hello ' + userName + '!')
+nameMe();
+
+function nameMe() {
+  let userName;
+  userName = prompt("What is your name?");
+  if (userName.length == 0) {
+    userName = "user";
+    console.log("Hello " + userName + "!");
+  } else if (userName.length == 1) {
+    alert("Your name can't be a single digit.  Please enter your name again.");
+    nameMe();
+  } else {
+    console.log("Hello " + userName + "!");
+  }
+
+  if (userName == "Nick" || userName == "nick") {
+    console.log("We're twinsies!" + " ;)");
+  }
+
+  document.write("Hi " + userName + "!");
+  document.write(" Let us help you get your new home.");
 }
 
-if (userName == 'Nick' || userName == 'nick') {
-  console.log('We\'re twinsies!' + ' \;\)');
+function buildHouses(){
+let userHouses = 0;
+  userHouses = prompt("How many houses do you want?");
+  for (let i=0;i <userHouses;i++){
+    const house = document.createElement("img");
+    house.src = "images/house.png";
+    document.body.appendChild(house);
+  }
 }
-
-document.write('Hi ' + userName + '!');
-document.write(' Let us help you get your new home.');
